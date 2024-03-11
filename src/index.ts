eb09b5e97f86ef4680e9e560e1e9ee7fff859665
narsima.chilkuri <narsima.chilkuri@aieze.in>
2024-03-11 23:49:03 +0530

    corrected exports

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,15 @@
-import {
-  redisUtils,
-  kafkaUtils,
-  mongoUtils,
-  postgresUtils,
-} from "./utils/database";
-import objectStorageUtils from "./utils/objectStorage";
-import loggerUtils from "./utils/logger/loggerUtils";
-import { envUtils } from "./utils/config";
-import { jwtUtils } from "./utils/jwt";
-import { encryptionUtils } from "./utils/encryption";
-import { common, cacheTTL, httpStatusCodes } from "./constants";
-
 export {
   redisUtils,
   kafkaUtils,
   mongoUtils,
   postgresUtils,
-  objectStorageUtils,
-  loggerUtils,
-  envUtils,
-  httpStatusCodes,
-  encryptionUtils,
-  jwtUtils,
-  cacheTTL,
-  common,
-};
+} from "./utils/database";
+
+export { loggerUtils } from "./utils/logger";
+
+export { common, cacheTTL, httpStatusCodes } from "./constants";
+
+export { envUtils } from "./utils/config";
+export { jwtUtils } from "./utils/jwt";
+export { encryptionUtils } from "./utils/encryption";
+export { objectStorageUtils } from "./utils/objectStorage";
--- src/utils/objectStorage/index.ts
+++ src/utils/objectStorage/index.ts
@@ -2,10 +2,8 @@ import * as doSpacesUtils from "./doSpacesUtils";
 import * as s3Utils from "./s3Utils";
 import * as minioUtils from "./minioUtils";
 
-const objectStorageUtils = {
+export const objectStorageUtils = {
   doSpaces: doSpacesUtils,
   s3: s3Utils,
   minio: minioUtils,
 };
-
-export default objectStorageUtils;
