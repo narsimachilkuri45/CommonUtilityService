3aacd3bfac94b0939ff8664459853aa1e195b478
narsima.chilkuri <narsima.chilkuri@aieze.in>
2024-03-17 10:20:44 +0530

    added expires in while generating jwt

--- src/utils/jwt/jwtUtils.ts
+++ src/utils/jwt/jwtUtils.ts
@@ -1,8 +1,8 @@
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../../constants/common";
 
-export function generateJwt(payload: any): string {
-  return jwt.sign(payload, JWT_SECRET);
+export function generateJwt(payload: any, expiresIn: number = 15 * 60): string {
+  return jwt.sign(payload, JWT_SECRET, { expiresIn });
 }
 
 export function verifyJwt(token: string): any {
